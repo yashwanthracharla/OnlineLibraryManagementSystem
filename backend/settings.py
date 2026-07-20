f0eca61c363ec58ae208df9ff719002a6a55988c
YASHWANTH RACHARLA <yashwanthracharla669@gmail.com>
2026-07-20 19:32:33 +0530

    Add vercel rewrite configuration

--- backend/settings.py
+++ backend/settings.py
@@ -190,14 +190,11 @@ REST_FRAMEWORK = {
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
-import cloudinary
 
-cloudinary.config(
-    cloud_name=config("CLOUDINARY_CLOUD_NAME"),
-    api_key=config("CLOUDINARY_API_KEY"),
-    api_secret=config("CLOUDINARY_API_SECRET"),
-)
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
